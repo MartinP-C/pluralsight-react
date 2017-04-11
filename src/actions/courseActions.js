ef7faf6df44345cb3a48ca784a4c32898bd8ca7a
Martin <martin@martin.com>
2017-04-11 10:51:21 +0100

    actions to const

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,8 @@
+import * as types from './actionsTypes';
+
 export function createCourse(course) {
     return {
-        type: 'CREATE_COURSE',
+        type: types.CREATE_COURSE,
         course
     };
 }
